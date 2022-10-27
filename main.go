41724f31562297a70621c65326739fa81e85c779
admin <timoniniv20@st.ithub.ru>
2022-10-27 15:24:33 +0300

    HA HA

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var myName string = "Гриша"
+var myName string = "Жопер"
 
 const tgApiUrl string = "https://api.telegram.org/" + "bot5794246977:AAE2ab_pZ97tyY1AoNHhjFqQIS0bg3ffYuA"
 
@@ -22,71 +22,10 @@ func main() {
 	go UpdateLoop()
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", Handler)
+	router.Handle("/", http.FileServer((http.Dir("./static/"))))
+	router.HandleFunc("/api", Handler)
 
 	http.ListenAndServe("localhost:8080", router)
-}
-
-type UpdateResponse struct {
-	Ok     bool           `json:"ok"`
-	Result []UpdateStruct `json:"result"`
-}
-type UpdateStruct struct {
-	Id                  int     `json:"update_id"`
-	Message             Message `json:"message"`
-	Edited_Message      Message `json:"edited_message"`
-	Channel_Post        Message `json:"channel_post"`
-	Edited_Channel_Post Message `json:"edited_channel_post"`
-}
-type Message struct {
-	Id   int    `json:"id"`
-	User User   `json:"from"`
-	Date int    `json:"date"`
-	Chat Chat   `json:"chat"`
-	Text string `json:"text"`
-}
-type SendMessage struct {
-	Chat_Id             int    `json:"chat_id"`
-	Text                string `json:"text"`
-	Reply_To_Message_Id int    `json:"reply_to_message_id"`
-}
-type User struct {
-	Id         int    `json:"id"`
-	Is_Bot     bool   `json:"is_bot"`
-	First_Name string `json:"first_name"`
-	Username   string `json:"username"`
-	Is_Prem    bool   `json:"is_premium"`
-}
-type Chat struct {
-	Id   int    `json:"id"`
-	Type string `json:"type"`
-}
-
-type MainStruct struct {
-	Ok     bool   `json:"ok"`
-	Result Result `json:"result"`
-}
-type Result struct {
-	Id         int    `json:"id"`
-	Is_bot     bool   `json:"is_bot"`
-	First_Name string `json:"first_name"`
-	// Username                    string `json:"username"`
-	// CanJoin_Groups              bool   `json:"can_join_groups"`
-	// Can_Read_All_Group_Messages bool   `json:"can_read_all_group_messages"`
-	// Supports_Inline_Queries     bool   `json:"supports_inline_queries"`
-	Abilities []string `json:"abilities"`
-}
-
-type WeatherResponse struct {
-	Weather []Weather `json:"weather"`
-	Main    Main      `json:"main"`
-}
-type Weather struct {
-	Description string `json:"description"`
-}
-type Main struct {
-	Temp_Min float32 `json:"temp_min"`
-	Temp_Max float32 `json:"temp_max"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -122,7 +61,7 @@ func UpdateLoop() {
 	lastId := 0
 	for {
 		lastId = Update(lastId)
-		time.Sleep(5 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -151,7 +90,7 @@ func Update(lastId int) int {
 		if splitedTxt[0] == myName {
 			switch strings.Split(splitedTxt[1], ": ")[0] {
 			case "танцуй!":
-				return SendMsg(lastId, ev, "Ты эбобо??"+strconv.Itoa(ev.Message.Chat.Id))
+				return SendMsg(lastId, ev, "Ты эбобо??")
 			case "шути!":
 				return SendMsg(lastId, ev, "Танцуют два негра и один упал.")
 			case "дай погоду!":
@@ -191,7 +130,7 @@ func Update(lastId int) int {
 			}
 		}
 
-		return SendMsg(lastId, ev, "Не понимаю((")
+		// return SendMsg(lastId, ev, "Не понимаю((")
 	}
 
 	return lastId
---
+++ structs.go
@@ -0,0 +1,63 @@
+package main
+
+type UpdateResponse struct {
+	Ok     bool           `json:"ok"`
+	Result []UpdateStruct `json:"result"`
+}
+type UpdateStruct struct {
+	Id                  int     `json:"update_id"`
+	Message             Message `json:"message"`
+	Edited_Message      Message `json:"edited_message"`
+	Channel_Post        Message `json:"channel_post"`
+	Edited_Channel_Post Message `json:"edited_channel_post"`
+}
+type Message struct {
+	Id   int    `json:"id"`
+	User User   `json:"from"`
+	Date int    `json:"date"`
+	Chat Chat   `json:"chat"`
+	Text string `json:"text"`
+}
+type SendMessage struct {
+	Chat_Id             int    `json:"chat_id"`
+	Text                string `json:"text"`
+	Reply_To_Message_Id int    `json:"reply_to_message_id"`
+}
+type User struct {
+	Id         int    `json:"id"`
+	Is_Bot     bool   `json:"is_bot"`
+	First_Name string `json:"first_name"`
+	Username   string `json:"username"`
+	Is_Prem    bool   `json:"is_premium"`
+}
+type Chat struct {
+	Id   int    `json:"id"`
+	Type string `json:"type"`
+}
+
+type MainStruct struct {
+	Ok     bool   `json:"ok"`
+	Result Result `json:"result"`
+}
+type Result struct {
+	Id         int    `json:"id"`
+	Is_bot     bool   `json:"is_bot"`
+	First_Name string `json:"first_name"`
+	// Username                    string `json:"username"`
+	// CanJoin_Groups              bool   `json:"can_join_groups"`
+	// Can_Read_All_Group_Messages bool   `json:"can_read_all_group_messages"`
+	// Supports_Inline_Queries     bool   `json:"supports_inline_queries"`
+	Abilities []string `json:"abilities"`
+}
+
+type WeatherResponse struct {
+	Weather []Weather `json:"weather"`
+	Main    Main      `json:"main"`
+}
+type Weather struct {
+	Description string `json:"description"`
+}
+type Main struct {
+	Temp_Min float32 `json:"temp_min"`
+	Temp_Max float32 `json:"temp_max"`
+}
